0ff205f615086bedd953d5cb71c0183d9c39ec88
Tireon003 <gunash29@mail.ru>
2024-10-16 02:37:58 +0300

    base config module

---
+++ api_server/config.py
@@ -0,0 +1,28 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str
+
+    DB_NAME: str
+    DB_HOST: str
+    DB_USER: str
+    DB_PORT: str
+    DB_PASS: str
+
+    API_HOST: str
+    API_PORT: int
+
+    @property
+    def db_url(self):
+        return (f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@"
+                f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}")
+
+    @property
+    def redis_url(self):
+        return "redis://redis_cache:6379"  #todo name docker container redis as host named here
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
