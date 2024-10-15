9c9301b63361a0a39e1d6174905a32edd560c96a
Tireon003 <gunash29@mail.ru>
2024-10-16 02:35:57 +0300

    implement celery task: notification from bot

---
+++ api_server/tasks/notification_tasks.py
@@ -0,0 +1,19 @@
+from celery import Celery
+from aiogram import Bot
+
+from api_server.config import settings
+
+
+celery = Celery(
+    "notifications",
+    broker=settings.redis_url
+)
+
+
+@celery.task
+async def send_notification_to_user(tg_chat_id: int, message: str) -> None:
+    bot = Bot(token=settings.BOT_TOKEN)
+    await bot.send_message(
+        chat_id=tg_chat_id,
+        text=message,
+    )
