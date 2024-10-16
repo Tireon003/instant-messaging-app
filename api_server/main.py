bcb205e75063d5bf9c2a79bcd3304b3aec6ac252
Tireon003 <gunash29@mail.ru>
2024-10-17 02:13:25 +0300

    allowed all origins in CORS and add exception handlers on app

--- api_server/main.py
+++ api_server/main.py
@@ -9,12 +9,10 @@ from api_server.exceptions import (
     NoSuchUserInDBException,
     WrongPasswordException,
     UserAlreadyExistException,
+    InvalidCodeException,
 )
 
-allowed_origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+allowed_origins = ["*"]
 
 
 app = FastAPI(
@@ -33,7 +31,7 @@ app.include_router(auth_router)
 
 
 @app.exception_handler(NoSuchUserInDBException)
-async def handle_expired_token_exception(request: Request, exc: NoSuchUserInDBException):
+async def handle_no_such_user_exception(request: Request, exc: NoSuchUserInDBException):
     return JSONResponse(
         status_code=status.HTTP_404_NOT_FOUND,
         content="User with such username doesn't exists",
@@ -42,7 +40,7 @@ async def handle_expired_token_exception(request: Request, exc: NoSuchUserInDBEx
 
 
 @app.exception_handler(WrongPasswordException)
-async def handle_expired_token_exception(request: Request, exc: WrongPasswordException):
+async def handle_wrong_pass_exception(request: Request, exc: WrongPasswordException):
     return JSONResponse(
         status_code=status.HTTP_401_UNAUTHORIZED,
         content="User with such username doesn't exists",
@@ -51,12 +49,22 @@ async def handle_expired_token_exception(request: Request, exc: WrongPasswordExc
 
 
 @app.exception_handler(UserAlreadyExistException)
-async def handle_expired_token_exception(request: Request, exc: UserAlreadyExistException):
+async def handle_user_already_exist_exception(request: Request, exc: UserAlreadyExistException):
     return JSONResponse(
         status_code=status.HTTP_400_BAD_REQUEST,
         content="User with such username already exists",
         headers={"WWW-Authenticate": "Bearer"}
     )
+
+
+@app.exception_handler(InvalidCodeException)
+async def handle_invalid_code_exception(request: Request, exc: InvalidCodeException):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content="Invalid code provided",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+
 
 if __name__ == "__main__":
     uvicorn.run(
