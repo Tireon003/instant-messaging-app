fb3e861741c55615fa65cd075bbe673afb7b1e08
Tireon003 <gunash29@mail.ru>
2024-10-17 02:11:13 +0300

    fixed issues in redis connection class

--- api_server/core/redis.py
+++ api_server/core/redis.py
@@ -1,5 +1,8 @@
-import aioredis
-from aioredis.client import Redis
+from typing import AsyncGenerator
+
+from redis import asyncio as aioredis
+from redis.asyncio import Redis as RedisInstance
+from contextlib import asynccontextmanager
 
 from api_server.config import settings
 
@@ -8,7 +11,15 @@ class Redis:
 
     URL = settings.redis_url
 
-    @classmethod
-    async def create_redis_connection(cls) -> Redis:
-        conn = await aioredis.from_url(cls.URL)
+    @asynccontextmanager
+    async def create_connection(self) -> AsyncGenerator[RedisInstance, None]:
+        conn = await aioredis.from_url(self.URL)
         yield conn
+        await conn.close()
+
+    async def get_connection(self) -> RedisInstance:
+        try:
+            async with self.create_connection() as conn:
+                return conn
+        except aioredis.ConnectionError as e:
+            print(f"Redis connection error: {e}")
