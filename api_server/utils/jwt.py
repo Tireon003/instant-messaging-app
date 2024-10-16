4070f7056a87a319684cf8d48db9dfdc25269d30
Tireon003 <gunash29@mail.ru>
2024-10-16 19:16:42 +0300

    jwt tool class

---
+++ api_server/utils/jwt.py
@@ -0,0 +1,27 @@
+import jwt
+import datetime as dt
+
+from api_server.config import settings
+from api_server.exceptions import ExpiredTokenException, InvalidTokenException
+
+
+class JwtTool:
+
+    ALG = "HS256"
+    SECRET = settings.JWT_SECRET
+
+    @classmethod
+    def create_token(cls, payload: dict):
+        return jwt.encode(
+            payload=payload,
+            key=cls.SECRET,
+            algorithm=cls.ALG
+        )
+
+    @classmethod
+    def read_token(cls, token: str) -> dict:
+        try:
+            payload = jwt.decode(token, cls.SECRET, algorithms=[cls.ALG])
+            return payload
+        except jwt.InvalidTokenError:
+            raise InvalidTokenException()
