8a99f9b13fdfc2c2bc86be6e35c35c375712cf26
Tireon003 <gunash29@mail.ru>
2024-10-17 02:10:15 +0300

    implement service dependency

--- api_server/depends.py
+++ api_server/depends.py
@@ -3,10 +3,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Callable
 
 from api_server.repositories import UserRepo
+from api_server.services import UserService
 
 
-def get_user_repository(get_async_session: Callable) -> Callable:
-    def _get_user_repository(session: AsyncSession = Depends(get_async_session)) -> UserRepo:
-        return UserRepo(session)
-
-    return _get_user_repository
+def get_user_service(get_async_session: Callable) -> Callable:
+    def _get_user_service(session: AsyncSession = Depends(get_async_session)) -> UserService:
+        user_repo = UserRepo(session)
+        return UserService(user_repo)
+    return _get_user_service
