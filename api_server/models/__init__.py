87b2c53fe83e653dfb670b57ff0732e4b486f087
Tireon003 <gunash29@mail.ru>
2024-10-18 01:41:56 +0300

    add Base model to init

--- api_server/models/__init__.py
+++ api_server/models/__init__.py
@@ -1,8 +1,10 @@
 from .chats import ChatsORM
 from .users import UsersORM
 from .messages import MessagesORM
+from .base import Base
 
 __all__ = (
+    "Base",
     "ChatsORM",
     "UsersORM",
     "MessagesORM",
