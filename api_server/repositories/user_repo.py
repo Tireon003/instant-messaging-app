6b14aa3541ddbe8d1d2a3b9b0f3a0d215ef4cd13
Tireon003 <gunash29@mail.ru>
2024-10-18 01:45:47 +0300

    new method to select users with provided tg_chat_id

--- api_server/repositories/user_repo.py
+++ api_server/repositories/user_repo.py
@@ -25,3 +25,12 @@ class UserRepo:
         new_user = UsersORM(**user_record.model_dump())
         self.__session.add(new_user)
         await self.__session.commit()
+
+    async def select_by_tg_chat_id(self, tg_chat_id: int) -> UsersORM | None:
+        stmt = (
+            select(UsersORM)
+            .filter_by(tg_chat_id=tg_chat_id)
+        )
+        result = await self.__session.scalars(stmt)
+        user = result.one_or_none()
+        return user
