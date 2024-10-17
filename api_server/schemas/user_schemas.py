f0f543f97c8809a98cfc6147cc79e8d2846935d7
Tireon003 <gunash29@mail.ru>
2024-10-18 01:42:39 +0300

    refactored UserLogin and UserSignup, add UserInsertToDB schema

--- api_server/schemas/user_schemas.py
+++ api_server/schemas/user_schemas.py
@@ -5,13 +5,18 @@ class BaseUser(BaseModel):
     username: str = Field(min_length=6, max_length=20)
 
 
-class UserLogin(BaseUser):
+class UserCredentials(BaseUser):
     password: str = Field(min_length=8, max_length=50)
 
 
-class UserSignup(BaseUser):
-    password: str = Field(min_length=8, max_length=50)
+class UserLogin(UserCredentials):
+    pass
 
 
-class UserInsertToDB(UserSignup):
+class UserSignup(UserCredentials):
+    pass
+
+
+class UserInsertToDB(BaseUser):
+    hashed_password: str
     tg_chat_id: int
