eb4f121bcf2429b7462d83d0689880be8d583ae3
Tireon003 <gunash29@mail.ru>
2024-10-18 01:48:09 +0300

    replaced getting login form to depends

--- api_server/depends.py
+++ api_server/depends.py
@@ -1,9 +1,11 @@
 from fastapi import Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
-from typing import Callable
+from typing import Callable, Annotated
 
 from api_server.repositories import UserRepo
 from api_server.services import UserService
+from api_server.schemas import UserCredentials
 
 
 def get_user_service(get_async_session: Callable) -> Callable:
@@ -11,3 +13,11 @@ def get_user_service(get_async_session: Callable) -> Callable:
         user_repo = UserRepo(session)
         return UserService(user_repo)
     return _get_user_service
+
+
+def get_login_form(credentials: Annotated[UserCredentials, Depends(OAuth2PasswordRequestForm)]):
+    data = UserCredentials(
+        username=credentials.username,
+        password=credentials.password
+    )
+    return data
