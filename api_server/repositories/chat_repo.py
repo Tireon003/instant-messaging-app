945103760d347ab66240bc4de7192402ebad6b06
Tireon003 <gunash29@mail.ru>
2024-10-20 17:26:40 +0300

    new methods to insert message, select chat, select chat list, insrt chat

--- api_server/repositories/chat_repo.py
+++ api_server/repositories/chat_repo.py
@@ -1,8 +1,13 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, or_, and_
+from sqlalchemy.orm import aliased
 
-from api_server.schemas import MessageCreateInDB
-from api_server.models import MessagesORM
+from api_server.schemas import (
+    MessageCreateInDB,
+    ChatCreate,
+    ChatAndRecipient,
+)
+from api_server.models import MessagesORM, ChatsORM, UsersORM
 
 
 class ChatRepo:
@@ -10,10 +15,13 @@ class ChatRepo:
     def __init__(self, session: AsyncSession) -> None:
         self.__session = session
 
-    async def insert_message(self, message: MessageCreateInDB) -> None:
+    async def insert_message(self, message: MessageCreateInDB) -> MessagesORM:
         new_message = MessagesORM(**message.model_dump())
         self.__session.add(new_message)
+        await self.__session.flush()
+        await self.__session.refresh(new_message)
         await self.__session.commit()
+        return new_message
 
     async def select_messages(self, chat_id: int) -> list[MessagesORM] | None:
         stmt = (
@@ -24,3 +32,69 @@ class ChatRepo:
         result = await self.__session.scalars(stmt)
         messages = [message for message in result.all()]
         return messages
+
+    async def select_chat(self, user_1: int, user_2: int) -> ChatsORM:
+        stmt = (
+            select(ChatsORM)
+            .where(
+                or_(
+                    and_(
+                        ChatsORM.user_1 == user_1,
+                        ChatsORM.user_2 == user_2,
+                    ),
+                    and_(
+                        ChatsORM.user_1 == user_2,
+                        ChatsORM.user_2 == user_1,
+                    ),
+                )
+            )
+        )
+        result = await self.__session.scalars(stmt)
+        chat = result.one_or_none()
+        return chat
+
+    async def select_chats(self,
+                           user_id: int) -> list[ChatAndRecipient] | None:
+        user_1_alias = aliased(UsersORM)
+        user_2_alias = aliased(UsersORM)
+        stmt = (
+            select(
+                ChatsORM.id.label("chat_id"),
+                user_1_alias.username.label("recipient_name"),
+                user_1_alias.id.label("recipient_id")
+            )
+            .join(
+                user_1_alias,
+                ChatsORM.user_1 == user_1_alias.id
+            )
+            .where(ChatsORM.user_2 == user_id)
+            .union_all(
+                select(
+                    ChatsORM.id.label("chat_id"),
+                    user_2_alias.username.label("recipient_name"),
+                    user_2_alias.id.label("recipient_id")
+                )
+                .join(
+                    user_2_alias,
+                    ChatsORM.user_2 == user_2_alias.id
+                )
+                .where(ChatsORM.user_1 == user_id)
+            )
+        )
+        result = await self.__session.execute(stmt)
+        chats = [
+            ChatAndRecipient(
+                chat_id=row.chat_id,
+                recipient_name=row.recipient_name,
+                recipient_id=row.recipient_id
+            ) for row in result.all()
+        ]
+        return chats
+
+    async def insert_chat(self, chat_schema: ChatCreate) -> ChatsORM:
+        new_chat = ChatsORM(**chat_schema.model_dump())
+        self.__session.add(new_chat)
+        await self.__session.flush()
+        await self.__session.refresh(new_chat)
+        await self.__session.commit()
+        return new_chat
