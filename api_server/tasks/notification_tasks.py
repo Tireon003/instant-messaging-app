ac8c35b9d52b0e78a198912cf04d503395f39e3f
Tireon003 <gunash29@mail.ru>
2024-10-20 17:22:49 +0300

    sending message logic outside task

--- api_server/tasks/notification_tasks.py
+++ api_server/tasks/notification_tasks.py
@@ -1,5 +1,6 @@
 from celery import Celery
 from aiogram import Bot
+import asyncio
 
 from api_server.config import settings
 
@@ -12,8 +13,20 @@ celery = Celery(
 
 
 @celery.task
-async def send_notification_to_user(tg_chat_id: int, message: str) -> None:
+def send_notification_to_user(tg_chat_id: int, message: str, from_user: str) -> None:
     bot = Bot(token=settings.BOT_TOKEN)
+    asyncio.run(send_message(
+        bot=bot,
+        tg_chat_id=tg_chat_id,
+        message=message,
+        from_user=from_user
+    ))
+
+
+async def send_message(bot: Bot, tg_chat_id: int, message: str, from_user: str) -> None:
+    message = (f"Пропущенное сообщение от пользователя {from_user}.\n"
+               f"\n"
+               f'"{message}"')
     await bot.send_message(
         chat_id=tg_chat_id,
         text=message,
