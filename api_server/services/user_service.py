afe6d1c0bb1464277e03abe0b23bc82d97a18691
Tireon003 <gunash29@mail.ru>
2024-10-20 17:25:47 +0300

    new methods to get data from db

--- api_server/services/user_service.py
+++ api_server/services/user_service.py
@@ -28,7 +28,7 @@ class UserService:
         )
 
     async def login_user(self, user_data: UserLogin) -> str:
-        match_user = await self.repo.select_user_by_username(username=user_data.username)
+        match_user = await self.repo.select_user_by_username(user_data.username)
         if not match_user:
             raise NoSuchUserInDBException()
         else:
@@ -42,6 +42,7 @@ class UserService:
                 redis = await self.redis.get_connection()
                 payload = {
                     "sub": match_user.id,
+                    "name": match_user.username,
                     "tg_chat_id": match_user.tg_chat_id,
                 }
                 jwt_token = JwtTool.create_token(payload=payload)
@@ -53,7 +54,7 @@ class UserService:
                 return jwt_token
 
     async def generate_registration_code(self, user_data: UserSignup) -> str:
-        match_user = await self.repo.select_user_by_username(username=user_data.username)
+        match_user = await self.repo.select_user_by_username(user_data.username)
         if match_user:
             raise UserAlreadyExistException()
         else:
@@ -70,7 +71,6 @@ class UserService:
 
     async def activate_user(self, code: str, tg_chat_id: int) -> None:
         redis = await self.redis.get_connection()
-        # todo: проверить что tg_chat_id уникален в базе, чтобы не регистрировать больше 1 юзера с одинаковым tg_chat_id
         user_signup_data_json_str = await redis.get(code)
         if not user_signup_data_json_str:
             raise InvalidCodeException()
@@ -95,3 +95,23 @@ class UserService:
         redis = await self.redis.get_connection()
         session_key = f"{user_id}:session"
         await redis.delete(session_key)  # todo check deletion of non-existing key
+
+    async def get_recipient_tg_chat_id(self, user_id: int) -> int | None:
+        user = await self.repo.select_user(user_id)
+        if not user:
+            raise NoSuchUserInDBException()
+        else:
+            tg_chat_id = user.tg_chat_id
+            return tg_chat_id
+
+    async def get_id_from_username(self, username: str) -> int | None:
+        user = await self.repo.select_user_by_username(username)
+        if user:
+            return user.id
+        else:
+            return None
+
+    async def get_username(self, user_id: int) -> str | None:
+        user = await self.repo.select_user(user_id)
+        if user:
+            return user.username
