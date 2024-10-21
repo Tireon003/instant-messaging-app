8d63a01ca630fe584d83155b8ecd9bc227590f95
Tireon003 <gunash29@mail.ru>
2024-10-22 01:43:16 +0300

    placed logic for message handling in websocket class

--- api_server/services/websocket.py
+++ api_server/services/websocket.py
@@ -2,6 +2,8 @@ from typing import Dict
 from fastapi import WebSocket
 import logging
 
+from api_server.services import ChatService, UserService
+
 logger = logging.getLogger(__name__)
 
 
@@ -20,6 +22,27 @@ class WebSocketManager:
         if user_id in self.active_connections:
             del self.active_connections[user_id]
             logger.info("User %s disconnected.", user_id)
+
+    @staticmethod
+    async def handle_messages(
+            websocket: WebSocket,
+            chat_service: ChatService,
+            user_service: UserService,
+            chat_id: int,
+            from_user: int,
+            to_user: int,
+    ) -> None:
+        sender_user = await user_service.get_user_from_db(from_user)
+        recipient_user = await user_service.get_user_from_db(to_user)
+        while True:
+            message = await websocket.receive_text()
+            message_json = await chat_service.send_message(
+                chat_id=chat_id,
+                from_user=sender_user,
+                to_user=recipient_user,
+                message=message.strip(),
+            )
+            yield message_json
 
     async def broadcast_personal_message(self, message: str, user_id: int):
         """Отправка сообщения конкретному пользователю по его user_id."""
