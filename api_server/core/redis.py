ec9cbf0a11571f6b63c738637bb22cfd5cc9ae5f
Tireon003 <gunash29@mail.ru>
2024-10-22 01:44:19 +0300

    fixed variables naming

--- api_server/core/redis.py
+++ api_server/core/redis.py
@@ -7,23 +7,24 @@ from contextlib import asynccontextmanager
 
 from api_server.config import settings
 
+
 logger = logging.getLogger(__name__)
 
 
 class Redis:
 
-    URL = settings.redis_url
+    REDIS_URL = settings.redis_url
 
     @asynccontextmanager
     async def create_connection(self) -> AsyncGenerator[RedisInstance, None]:
-        conn = await aioredis.from_url(self.URL)
-        yield conn
-        await conn.close()
+        connection = await aioredis.from_url(self.REDIS_URL)
+        yield connection
+        await connection.close()
 
     async def get_connection(self) -> RedisInstance:
         try:
-            async with self.create_connection() as conn:
+            async with self.create_connection() as connection:
                 logger.info("Created redis connection.")
-                return conn
+                return connection
         except aioredis.ConnectionError as e:
             logger.error("Error while connecting to redis: %s", e)
