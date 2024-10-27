ef205e40540aacda9918f1dd323348aec19159c9
Tireon003 <gunash29@mail.ru>
2024-10-27 19:09:42 +0300

    fix: logging line no

--- api_server/logs.py
+++ api_server/logs.py
@@ -5,5 +5,5 @@ def configure_logging(level=logging.INFO):
     logging.basicConfig(
         level=level,
         datefmt='%Y-%m-%d %H:%M:%S',
-        format='[%(asctime)s.%(msecs)03d] %(module)20s:(lineno)-3d %(levelname)-7s - %(message)s',
+        format='[%(asctime)s.%(msecs)03d] %(module)20s:%(lineno)-3d %(levelname)-7s - %(message)s',
     )
